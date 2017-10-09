6e331f14af48c3ea9afe9cf15cfd41f40c203aed
David Hale and Riki Montgomery <david@>
2017-10-09 14:23:43 -0700

    display on click

--- js/journal-interface.js
+++ js/journal-interface.js
@@ -7,20 +7,21 @@ $(document).ready(function() {
     var title = $('#title').val();
     var body = $('#body').val();
     var simpleEntry = new Entry(title, body);
-    var count = simpleEntry.wordCount(body);
-    var vowel = simpleEntry.getVowel(body);
-    var consonant = simpleEntry.getConsonants(body);
+    // var count = simpleEntry.wordCount(body);
+    // var vowel = simpleEntry.getVowel(body);
+    // var consonant = simpleEntry.getConsonants(body);
     var teaser = simpleEntry.getTeaser(body);
-    $('#journalEntry').append("<li><h3>" + title + "</h3></li>");
-    $('#journalEntry').append("<li><p>" + teaser + "</p></li>");
-    $('#bodyCopy').append(body);
-    $('#wordCount').append(count);
-    $('#vowelCount').append(vowel);
-    $('#consonantCount').append(consonant);
-  });
+    $('#titleName').append('<li><h3><span class="teaser">' + simpleEntry.title + '</span></h3></li>');
+    $('#teaserName').append("<li>" + teaser + "</li>");
+    // $('#teaserTitle').show();
 
-  $('#journalEntry').click(function(event) {
-    event.preventDefault();
-    $('#counters').show(800);
+    $('.teaser').last().click(function() {
+      $('#bodyCopy').text(simpleEntry.body);
+      $('#wordCount').text(simpleEntry.wordCount(simpleEntry.body));
+      $('#vowelCount').text(simpleEntry.getVowel(simpleEntry.body));
+      $('#consonantCount').text(simpleEntry.getConsonants(simpleEntry.body));
+      $('#teaserName').hide(800);
+      $('#counters').show(800);
+    });
   });
 });
